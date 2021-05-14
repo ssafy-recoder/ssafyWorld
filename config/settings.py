1e4c283d50e069246d16f7457a209d4fa13e7a53
mijung-oh <ks03102@naver.com>
2021-05-14 11:17:26 +0900

    Modify first url && requirements

--- config/settings.py
+++ config/settings.py
@@ -139,6 +139,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = 'accounts.User'
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('homepage.urls')),
-    path('accounts/', include('accounts.urls'))
+    path('', include('accounts.urls')),
+    path('homepage/', include('homepage.urls'))
 ]
