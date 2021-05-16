d2a09c8892d35c7c38c3820947468b05c34952db
blessisu <blessisu@naver.com>
2021-05-17 05:07:29 +0900

    Feat comment

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class HomeComment(models.Model):
+    content = models.CharField(max_length=20, null=False, blank=False)
+    writer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="writer")
+    host = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
