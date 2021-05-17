d7979651935ccd50dd74a9b3d35330d316a3ef46
unknown <photoyjphoto@naver.com>
2021-05-17 16:12:55 +0900

    Modify albums

--- albums/forms.py
+++ albums/forms.py
@@ -2,6 +2,14 @@ from django import forms
 from .models import Photo, Comment
 
 class CommentForm(forms.ModelForm):
+    label='댓글',
+    widgets=forms.Textarea(
+        attrs={
+            'rows':3,
+            'cols': 100,
+            'width':'200px',
+        }
+    )
 
     class Meta:
         model = Comment
--- albums/models.py
+++ albums/models.py
@@ -1,14 +1,18 @@
 from django.db import models
-
+from django.db.models.deletion import CASCADE
+from django.conf import settings
 # Create your models here.
+
 class Category(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
 
+
     def __str__(self):
-        return f'{self.group}'
+        return f'{self.name}'
 
 
 class Photo(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='photo')
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
