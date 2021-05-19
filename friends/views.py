745e05a8f4189861401c800fc91b6c824916d86d
opeerats <opeerats@naver.com>
2021-05-19 22:03:09 +0900

    revise/friends

--- friends/views.py
+++ friends/views.py
@@ -5,12 +5,18 @@ from .models import Location
 
 # Create your views here.
 def index(request, username):
-    person = get_object_or_404(get_user_model(), username=username)
+    host = get_object_or_404(get_user_model(), username=username)
     all_users = User.objects.all()
 
+    users = User.objects.all()
+    list_friends = []
+    for user in users:
+        if user.followers.filter(pk=host.pk).exists():
+            list_friends.append(user)
+
     context = {
-        'person': person,
-        'this_user': person,
+        'host': host,
+        'list_friends': list_friends,
         'all_users':all_users,
     }
     
@@ -18,17 +24,17 @@ def index(request, username):
 
 
 def recommend(request, username):
-    person = get_object_or_404(get_user_model(), username=username)
+    host = get_object_or_404(get_user_model(), username=username)
     
     users = User.objects.all()
     list_friends = []
     for user in users:
-        if user.followers.filter(pk=person.pk).exists():
+        if user.followers.filter(pk=host.pk).exists():
             list_friends.append(user)
     list_recommend = {}
     for friend in list_friends:
         for user in users:
-            if user != person and user not in list_friends:
+            if user != host and user not in list_friends:
                 if user.followers.filter(pk=friend.pk).exists():
                     if user not in list_recommend:
                         list_recommend[user] = 1
@@ -37,8 +43,7 @@ def recommend(request, username):
     sdict = sorted(list_recommend.items(), key=lambda x: x[1], reverse=True)
     sdict = dict(sdict)
     context = {
-        'person': person,
-        'this_user': person,
+        'host': host,
         'list_friends': list_friends,
         'list_recommend': sdict,
     }
@@ -46,27 +51,27 @@ def recommend(request, username):
 
 
 def location(request, username):
-    person = get_object_or_404(get_user_model(), username=username)
+    host = get_object_or_404(get_user_model(), username=username)
     key_js = 'cf38b368117baa7591dbe1e159585d8c'
     loca = Location.objects.all()
     context = {
         'key_js': key_js,
-        'this_user': person,
+        'host': host,
         'loca': loca,
     }
     return render(request, 'friends/location.html', context)
 
 
 def save_location(request, username):
-    person = get_object_or_404(get_user_model(), username=username)
+    host = get_object_or_404(get_user_model(), username=username)
     key_js = 'cf38b368117baa7591dbe1e159585d8c'
     latitude_submit = request.POST.get('latitude_submit')
     longitude_submit = request.POST.get('longitude_submit')
     print(latitude_submit, longitude_submit)
-    location = Location(user=person, latitude=latitude_submit, longitude=longitude_submit)
+    location = Location(user=host, latitude=latitude_submit, longitude=longitude_submit)
     location.save()
     context = {
         'key_js': key_js,
-        'this_user': person,
+        'host': host,
     }
     return render(request, 'friends/location.html', context)
